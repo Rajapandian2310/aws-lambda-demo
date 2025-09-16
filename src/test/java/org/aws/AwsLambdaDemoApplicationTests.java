5a407e7c3d9bd51a10c77adaf75d94e6e303d4ca
Rajapandian2310 <rapas1023@gmail.com>
2025-09-16 21:37:51 +0530

    first commit

---
+++ src/test/java/org/aws/AwsLambdaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLambdaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
